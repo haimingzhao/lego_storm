d2a3c9f0f4ef9adae8a51964f2f17f5666979bb1
Ashley Hemingway <ashley.hemingway12@imperial.ac.uk>
2015-02-17 17:20:04 +0000

    Fixed runtime errors in localisation

--- localisation.py
+++ localisation.py
@@ -1,7 +1,18 @@
 import time
 import math
+import random
 
-class robot:
+class localisation:
+
+	global NUM_OF_PARTS
+	global X
+	global Y
+	global THETA
+	global LINEAR_DISTANCE
+	global LINEAR_ROTATION
+	global ROTATION
+	global origin
+	global draw
 
 	NUM_OF_PARTS = 100
 
@@ -20,9 +31,10 @@ class robot:
 
 	draw = False
 
-	def __init__(self, draw=False, record=False):
+	def __init__(self, drawing=False, record=False):
 		self.particles = origin * NUM_OF_PARTS
-		draw = draw
+		global draw
+		draw = drawing
 		if record:
 			self.record_all = self.particles
 			self.record = True
@@ -36,40 +48,40 @@ class robot:
 		line = (x1, y1, x2, y2)
 		print "drawLine:" + str(line)
 
-	def ran_gauss(sigma):
+	def ran_gauss(self, sigma):
 		return random.gauss(0, sigma)
 
 	def degreeToRad(self, degree):
 		return degree * math.pi / 180
 
-	def update_particle_distance(pid, distance):
-		e = ran_gauss(LINEAR_DISTANCE)
-		f = ran_gauss(LINEAR_ROTATION)
+	def update_particle_distance(self, pid, distance):
+		e = self.ran_gauss(LINEAR_DISTANCE)
+		f = self.ran_gauss(LINEAR_ROTATION)
 		x = self.particles[pid][X] + (distance*e)*math.cos(self.particles[pid][THETA])
 		y = self.particles[pid][Y] + (distance*e)*math.sin(self.particles[pid][THETA])
 		theta = self.particles[pid][THETA] + f
 		self.particles[pid] = (x,y,theta)
 
-	def update_particle_rotation(pid, angle):
-		g = ran_gauss(degreeToRad(ROTATION))
+	def update_particle_rotation(self, pid, angle):
+		g = self.ran_gauss(self.degreeToRad(ROTATION))
 		x = self.particles[pid][X]
 		y = self.particles[pid][Y]
 		theta = self.particles[pid][THETA] + angle + g
 		self.particles[pid] = (x,y,theta)
 
-	def loc_distance(d):
+	def loc_distance(self, d):
 		if self.record: self.record_all += self.particles
 		for p in range(NUM_OF_PARTS):
-			update_particle_distance(p, d)
+			self.update_particle_distance(p, d)
 		if draw: draw_particles(self.particles)
 
-	def loc_rotation(angle):
+	def loc_rotation(self, angle):
 		if self.record: self.record_all += self.particles
 		for p in range(NUM_OF_PARTS):
-			update_particle_rotation(p, angle)
+			self.update_particle_rotation(p, angle)
 		if draw: draw_particles(self.particles)
 
-	def draw_all():
+	def draw_all(self):
 		draw_particles(self.record_all)
 
 
