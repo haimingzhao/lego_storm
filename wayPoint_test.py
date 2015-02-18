fb7eecd7ce081aceb45374d33fbaa0780f3a1c92
Dave Liskevich <dsl12@ic.ac.uk>
2015-02-18 17:31:06 +0000

    Indendation problem

--- wayPoint_test.py
+++ wayPoint_test.py
@@ -6,16 +6,16 @@ while True:
 	x,y = raw_input("Enter x and y coordinates to travel to or ctrl+c to quit").split()
 	x = float(x)
 	y = float(y)
-    navigateToWaypoint(x,y)
+	navigateToWaypoint(x,y)
 robot.terminate()
 
 def navigateToWaypoint(x,y):
 	particle = robot.get_loc().getAverage(robot)
-	currentX = particles[0]
-	currentY = particles[1]
-	theta = particles[3]
-	dx = x - currentX
-	dy = y - currentY
+	currentX = particle[0]
+	currentY = particle[1]
+	theta = particle[3]
+	dx = (100 *x) - currentX
+	dy = (100 *y) - currentY
 	alpha = atan2(dy,dx)
 	beta = normalize(alpha - theta,-math.pi,math.pi)
 	distance = math.hypot(dx, dy)
@@ -28,5 +28,5 @@ def normalise(angle):
 		return angle + (2 * math.pi)
 	if angle > math.pi:
 		return angle - (2 * math.pi)
-    else :
-    	return angle
+	else :
+		return angle
