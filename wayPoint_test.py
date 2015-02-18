bc98487fd54f0f106111c88caa64686310da879f
Dave Liskevich <dsl12@ic.ac.uk>
2015-02-18 17:23:37 +0000

    correcting syntax errors

--- wayPoint_test.py
+++ wayPoint_test.py
@@ -1,10 +1,19 @@
 from robot import robot
 import math
 
-robot = robot();
+robot = robot()
+while True:
+	x,y = raw_input("Enter x and y coordinates to travel to or ctrl+c to quit").split()
+	x = float(x)
+	y = float(y)
+    navigateToWaypoint(x,y)
+robot.terminate()
 
 def navigateToWaypoint(x,y):
-	particle = getAvgParticles
+	particle = robot.get_loc().getAverage(robot)
+	currentX = particles[0]
+	currentY = particles[1]
+	theta = particles[3]
 	dx = x - currentX
 	dy = y - currentY
 	alpha = atan2(dy,dx)
