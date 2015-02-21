cd9dbcd07cfc0e40f0975bad5534e2eaa75c598e
Ashley Hemingway <ashley.hemingway12@imperial.ac.uk>
2015-02-21 18:27:11 +0000

    Working waypoint for mcl

--- prac-files/wayPoint_mcl.py
+++ prac-files/wayPoint_mcl.py
@@ -1,30 +1,15 @@
 import sys
-import math
 sys.path.insert(0, '../')
 from robot import robot
 
-def navigateToWaypoint(newX, newY):
-	particle = robot.get_loc().get_average()
-	currentX = particle[0]
-	currentY = particle[1]
-	theta = particle[2]
-	dx = (100 * newX) - currentX
-	dy = (100 * newY) - currentY
-	alpha = math.degrees(math.atan2(dy,dx))
-	beta = normalise(alpha - theta)
-	distance = math.hypot(dx, dy)
-	robot.turnDeg(beta)
-	robot.forward(distance,False)
-	print robot.get_loc().get_average()
-
-def normalise(angle):
-	if angle < 0:
-		return normalise(angle + 360)
-	if angle > 360 :
-		return normalise(angle - 360)
-	else :
-		return angle
-
+positions = [(180, 30),
+             (180, 54),
+             (126, 54),
+             (126, 168),
+             (126, 126),
+             (30, 54),
+             (84, 54),
+             (84, 30)]
 
 robot = robot()
 
@@ -32,14 +17,8 @@ robot.enableBumper()
 
 # remember to change origin in localisation to (84,30)
 
-navigateToWaypoint(180, 30)
-navigateToWaypoint(180, 54)
-navigateToWaypoint(126, 54)
-navigateToWaypoint(126, 168)
-navigateToWaypoint(126, 126)
-navigateToWaypoint(30, 54)
-navigateToWaypoint(84, 54)
-navigateToWaypoint(84, 30)
-
+for p in positions:
+    x, y = p
+    robot.navigateToWaypoint(x, y)
 
 robot.terminate()
