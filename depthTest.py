0a428e5b17a9ce1ea8e6e5712cf38c886ef0e16e
IAmPi <pi@raspberrypi.(none)>
2015-02-21 19:41:25 +0000

    I'm a god

---
+++ depthTest.py
@@ -0,0 +1,15 @@
+from robot import robot
+
+r = robot()
+l = r.get_loc()
+p = (84,30,0)
+
+
+while True:
+	x = float(input("enter x of particle : "))
+	y = float(input("enter y of particle : "))
+	theta = float(input("enter theta of particle : "))
+			
+	m = l.getDepthMeasurement((x,y,theta))
+
+	print m
--- wallMap.py
+++ wallMap.py
@@ -1,3 +1,4 @@
+import math
 
 WALLS = [(0,0,0,168), (0,168,84,168), (84,126,84,210),
 	 (84,210,168,210), (168,210,168,84), (168,84,210,84),
@@ -11,8 +12,22 @@ class WallMap:
         self.walls += WALLS
 
     def isOnWall(self, point, wall):
-        pass
-	
+	wallA = (wall[0],wall[1])
+	wallB = (wall[2],wall[3])	
+
+        # Distance between point and starting points of the wall
+        distWallA = self.distanceBetweenPoints(point, wallA)
+        distWallB = self.distanceBetweenPoints(point, wallB)
+
+        # Length of the wall
+        wallLength = self.distanceBetweenPoints(wallA, wallB)
+	return distWallA + distWallB == wallLength	
+
+    def distanceBetweenPoints(self, p1, p2):
+	x1,y1 = p1
+	x2,y2 = p2
+	return math.hypot(x2 - x1, y2 - y1) 
+
     def clear(self):
         self.walls = []
 
